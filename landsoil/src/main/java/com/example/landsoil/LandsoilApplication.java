c0282d3a36edb724d51fccdfc79d446232c84c3b
clarisse uwimana <clarisseuwimana31@gmail.com>
2026-02-27 12:31:29 +0200

    Added landsoil folder

---
+++ landsoil/src/main/java/com/example/landsoil/LandsoilApplication.java
@@ -0,0 +1,13 @@
+package com.example.landsoil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandsoilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandsoilApplication.class, args);
+    }
+
+}
---
+++ landsoil/src/test/java/com/example/landsoil/LandsoilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.landsoil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandsoilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
